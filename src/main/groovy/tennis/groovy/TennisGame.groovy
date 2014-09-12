3cd03de3a9c5c9911a8d5e68a86ee5f092f290f9
Marek Chowaniok <marek.chowaniok@gmail.com>
2014-09-12 10:34:11 +0200

    groovy solution

--- src/main/groovy/tennis/groovy/TennisGame.groovy
+++ src/main/groovy/tennis/groovy/TennisGame.groovy
@@ -6,17 +6,56 @@ package tennis.groovy
  * Time: 9:04
  */
 class TennisGame {
-    TennisGame(String player1, String player2) {
+    def player1
+    def player2
+    def player1Name
+    def player2Name
 
+    TennisGame(String player1Name, String player2Name) {
+        this.player1Name = player1Name
+        this.player2Name = player2Name
+        this.player1 = 0
+        this.player2 = 0
     }
 
     String getScore() {
-        return null;
+        def result = "default"
+        if (player1.equals(player2)) {
+            if (player1 == 0)   result = "Love all"
+            else if (player1 == 1)  result = "Fifteen all"
+            else if (player1 == 2)  result = "Thirty all"
+            else if (player1 == 3 || player1 == 4)  result = "Deuce"
+        } else {
+            result = getNumberInText()
+        }
+        return result
+    }
+
+    String getNumberInText() {
+        def score = player1 > player2 ? player1 : player2
+        def playerName = player1 > player2 ? player1Name : player2Name
+        def result = ""
+        if (score == 1) {
+            result = "Fifteen,Love"
+        } else if (score == 2) {
+            result = "Love,Thirty"
+        } else if (score == 3) {
+            result = "Forty,Love"
+        } else if (score == 4) {
+            result = "${playerName} wins"
+        } else if (score == 5) {
+            result = "Advantage ${playerName}"
+        } else if (score > 5 ) {
+            if (Math.abs(player1 - player2) == 2) result = "${playerName} wins"
+        }
+        return result
     }
 
     void playerOneScores() {
-        
+        player1 += 1
     }
 
-    void playerTwoScores() {}
+    void playerTwoScores() {
+        player2 += 1
+    }
 }
